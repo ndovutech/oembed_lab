22fa8a9439c22751b5b35d9ecf8b7ca8dba27a9c
Noel Bacani <mail@ndovutech.com>
2015-06-09 18:41:08 -0700

    playing around with an embedded youtube player

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -1,2 +1,7 @@
 module ToolsHelper
+  require 'oembed'
+  def oembed_youtube_html(url)
+    resource = OEmbed::Providers::Youtube.get("http://www.youtube.com/watch?v=2BYXBC8WQ5k")
+    raw(resource.html)
+  end
 end
